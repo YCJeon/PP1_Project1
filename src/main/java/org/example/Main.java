52f3dc0e24f45593a96e623a7120ae0040a2d3f7
YCJeon <126933039+YCJeon@users.noreply.github.com>
2023-09-01 16:51:01 +0900

    Create Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("21900642", "홍길동");
+        System.out.println(s1.toString());
+    }
+}
